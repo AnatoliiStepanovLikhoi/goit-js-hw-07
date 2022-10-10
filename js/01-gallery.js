3e54165fb4d13e113602ac8c18bf9401d93e0afe
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-10-10 14:26:25 +0300

    basic light done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,85 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+let modalRef = null;
+
+const galleryDivRef = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryDivRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryDivRef.addEventListener('click', onPictureClick)
+
+// console.log(galleryDivRef);
+
+function createGalleryItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+      </a>
+</div>`
+    })
+        .join('');
+};
+
+function onPictureClick(event) {
+    // console.log(event.target)
+    event.preventDefault()
+
+    const onTargetClick = event.target;
+
+    // console.log(onPictureClick)
+
+    if (onTargetClick.nodeName !== 'IMG') {
+        return;
+    }
+
+    modalRef = basicLightbox.create(`
+    <img src="${onTargetClick.dataset.source}" 
+    alt="${onTargetClick.alt}">`)
+    
+    modalRef.show();
+    
+    window.addEventListener('keydown', onEscapeClose);
+}
+
+function onEscapeClose(event) {
+    if (event.code !== 'Escape') {
+        return
+    }
+
+    modalRef.close(listenerRemove)
+}
+
+function listenerRemove() {
+    window.removeEventListener('keydown', onEscapeClose);
+}
+
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show()
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
